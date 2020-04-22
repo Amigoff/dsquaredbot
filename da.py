8710cc4524e96dfe97503431bb48c0cef75a6ff7
ax0107 <alexandr.sh.0107@mail.ru>
2020-04-22 13:13:17 +0300

    Added try-except

--- da.py
+++ da.py
@@ -14,39 +14,45 @@ async def da(ctx, arg):
 
 @client.command(pass_context=True)
 async def pizda(ctx):
-    print('Вызвана команда "пизда"')
-    guild = ctx.message.guild
-    voice_client = guild.voice_client
-    await voice_client.disconnect()
-    await ctx.send("Откинулся Аркадий")
+    try:
+        print('Вызвана команда "пизда"')
+        guild = ctx.message.guild
+        voice_client = guild.voice_client
+        await voice_client.disconnect()
+        await ctx.send("Откинулся Аркадий")
+    except Exception as e:
+        ctx.send(f'Ошибочка бля {e}')
 
 
 @client.command(pass_context=True)
 async def manda(ctx, url):
-    channel = ctx.author.voice.channel
-    voice = await channel.connect()
-    print('Вызвана команда "манда"')
+    try:
+        channel = ctx.author.voice.channel
+        voice = await channel.connect()
+        print('Вызвана команда "манда"')
 
-    await ctx.send("Зделаю дарагой ежжи")
-    songthere = os.path.isfile("song.mp3")
-    if songthere:
-        os.remove("song.mp3")
+        await ctx.send("Зделаю дарагой ежжи")
+        songthere = os.path.isfile("song.mp3")
+        if songthere:
+            os.remove("song.mp3")
 
-    ydl_opts = {
-        "format": "bestaudio/beat",
-        "postprocessors": [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192",
-        }],
-    }
+        ydl_opts = {
+            "format": "bestaudio/beat",
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }],
+        }
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    for file in os.listdir("./"):
-        print(f'Тут файл: {file}')
-        if file.endswith(".mp3"):
-            name = file
-            os.rename(file, "song.mp3")
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        for file in os.listdir("./"):
+            print(f'Тут файл: {file}')
+            if file.endswith(".mp3"):
+                name = file
+                os.rename(file, "song.mp3")
+        voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    except Exception as e:
+        ctx.send(f'Ошибочка бля {e}')
 client.run(tok)
