9f27cc539f9fbd2f4d1d5856a1f1e831f3f0658f
ax0107 <alexandr.sh.0107@mail.ru>
2020-04-22 13:06:12 +0300

    Added logs

--- da.py
+++ da.py
@@ -5,19 +5,27 @@ import os
 tok = "NzAyMTM5MjM5MTIyNDY4OTc0.Xp__5Q.HCQ_UQLz-2irr6xGt0_L9-NNkn4"
 client = commands.Bot(command_prefix='!')
 
+
 @client.command(pass_context=True)
 async def da(ctx, arg):
+    print('Вызвана команда "да"')
     await ctx.send("хуйню базаришь")
+
+
 @client.command(pass_context=True)
 async def pizda(ctx):
+    print('Вызвана команда "пизда"')
     guild = ctx.message.guild
     voice_client = guild.voice_client
     await voice_client.disconnect()
     await ctx.send("Откинулся Аркадий")
+
+
 @client.command(pass_context=True)
 async def manda(ctx, url):
     channel = ctx.author.voice.channel
     voice = await channel.connect()
+    print('Вызвана команда "манда"')
 
     await ctx.send("Зделаю дарагой ежжи")
     songthere = os.path.isfile("song.mp3")
@@ -36,6 +44,7 @@ async def manda(ctx, url):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
     for file in os.listdir("./"):
+        print(f'Тут файл: {file}')
         if file.endswith(".mp3"):
             name = file
             os.rename(file, "song.mp3")
