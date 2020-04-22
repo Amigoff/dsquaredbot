58b77afccf1f54b5021e0249a4eb7ac68ac959dc
ax0107 <alexandr.sh.0107@mail.ru>
2020-04-22 14:52:45 +0300

    fixed

--- da.py
+++ da.py
@@ -33,8 +33,6 @@ async def pizda(ctx):
 @client.command(pass_context=True)
 async def manda(ctx, url):
     # try:
-    channel = ctx.author.voice.channel
-    voice = await channel.connect()
     print('Вызвана команда "манда"')
     await ctx.send("Зделаю дарагой ежжи")
     songthere = os.path.isfile("song.mp3")
@@ -57,13 +55,24 @@ async def manda(ctx, url):
         if file.endswith(".mp3"):
             name = file
             os.rename(file, "song.mp3")
-    player = voice.create_ffmpeg_player('vuvuzela.mp3', after=lambda: print('done'))
-    player.start()
-    while not player.is_done():
-        await time.sleep(1)
-    # disconnect after the player has finished
-    player.stop()
-    await voice.disconnect()
+
+    user = ctx.message.author
+    voice_channel = user.voice.voice_channel
+    channel = None
+    # only play music if user is in a voice channel
+    if voice_channel != None:
+        # grab user's voice channel
+        channel = voice_channel.name
+        await client.say('User is in channel: ' + channel)
+        # create StreamPlayer
+        vc = await client.join_voice_channel(voice_channel)
+        player = vc.create_ffmpeg_player('song.mp3', after=lambda: print('done'))
+        player.start()
+        while not player.is_done():
+            await time.sleep(1)
+        # disconnect after the player has finished
+        player.stop()
+        await vc.disconnect()
     # voice.play(discord.FFmpegPCMAudio("song.mp3"))
     # except Exception as e:
     #     await ctx.send(f'уа уа Ошибочка бля {e}')
