699f84aef5b851fd26bca231da6d632df8b5c3d8
Amigoff <64126522+Amigoff@users.noreply.github.com>
2020-12-03 14:44:33 +0300

    Update da.py

--- da.py
+++ da.py
@@ -116,7 +116,18 @@ async def anal(ctx, arg=None):
     vid = spisAk[num]
     lst1.append(vid)
     await play()
-    
+
+@client.command(pass_context=True)
+async def vader(ctx, arg=None):    
+    global lst1
+    global voice
+    try:
+        channel = ctx.author.voice.channel
+        voice = await channel.connect()
+    except:
+        pass
+    lst1.append("https://www.youtube.com/watch?v=U49RgwMSHc0")
+    await play()
 
 
 @client.command(pass_context=True)
