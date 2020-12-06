71bf358c1c6f7970aa806b565f95a832bccffdfe
Amigoff <64126522+Amigoff@users.noreply.github.com>
2020-12-06 16:54:14 +0300

    Update da.py

--- da.py
+++ da.py
@@ -423,7 +423,7 @@ async def play():
             else:
                 arg = " ".join(ur)
                 print(arg)
-                ydl("ytsearch: arg")
+                ydl "ytsearch: arg"
             for file in os.listdir("./"):
                 print(f'Тут файл: {file}')
                 if file.endswith(".mp3"):
